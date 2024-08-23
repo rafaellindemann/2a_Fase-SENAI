53e13598a39a43896807cfe9b3823050324b0365
rafaellindemann <rafaelld@gmail.com>
2024-08-23 17:16:12 -0300

    Criação do primeiro projeto com React bonitão

---
+++ oiReact/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <h1>Dinossauros</h1>
+        <h2>Carnívoros</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus nostrum praesentium suscipit porro cum possimus obcaecati blanditiis ullam placeat. Numquam mollitia magnam dolores est? Quam cumque exercitationem vel officia ipsum.lorem</p>
+    </>
+  )
+}
+
+export default App
